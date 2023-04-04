7c8b398f1317f3fc3749e139b9a357ca33b8d2c3
Roberto <robertog.adm@gmail.com>
2023-04-04 15:31:38 -0300

    Preparando o hambiente de desenvolvimento

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Projects from '../pages/Projetos/Projetos';
+
+function Routes() {
+  return (
+  <Switch>
+    <Route path="/" component={ Home } exact />
+    <Route path="/projets" component={ Projects } />
+  </Switch>
+  );
+}
+
+export default Routes;
